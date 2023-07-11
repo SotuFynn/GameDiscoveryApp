aad654277196debca4e740507035e156da1468bb
SotuFynn <fynnwillenborg@googlemail.com>
2023-07-11 13:45:49 +0200

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,7 @@ export interface Game {
     name: string;
     background_image: string; 
     parent_platforms: {platform: Platform}[] //Array of objects where each object has a property called Platform of type platform
+    metacritic: number;
   }
   
   interface FetchGamesResponse {
