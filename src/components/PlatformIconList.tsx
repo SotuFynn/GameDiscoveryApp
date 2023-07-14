34b130919dea1bf3d4737cc897babd28fcfd3b02
SotuFynn <fynnwillenborg@googlemail.com>
2023-07-14 12:35:58 +0200

    Final removal of import statements

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -10,7 +10,7 @@ import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { Platform } from "../hooks/useGames";
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
