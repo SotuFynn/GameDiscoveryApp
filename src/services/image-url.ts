5e9d76d95b271039d0916f97dba6ad28215410d1
SotuFynn <fynnwillenborg@googlemail.com>
2023-07-14 08:59:22 +0200

    Handle games w/o image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,9 @@
+import noImage from '../assets/noImage.webp';
+
 const getCroppedImageUrl = (url: string) => {
 
-    if(!url) return '';
+    if(!url) return noImage;
+
     const target='media/';
 const index = url.indexOf(target) + target.length;
 return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
